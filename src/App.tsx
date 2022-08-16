3271fe40eb5d0a325d47f746f52bec0a5ed3739f
nguyennguyen1112000 <nguyennguyen1112000@gmail.com>
2022-08-17 00:13:19 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr'
+import { Button, Input, notification } from 'antd'
 function App() {
+  const [connection, setConnection] = useState<null | HubConnection>(null)
+  const [inputText, setInputText] = useState('')
+
+  useEffect(() => {
+    const connect = new HubConnectionBuilder()
+      .withUrl('https://localhost:7184/notification')
+      .withAutomaticReconnect()
+      .build()
+
+    setConnection(connect)
+  }, [])
+  const sendMessage = async () => {
+    if (connection) await connection.send('SendMessage', inputText)
+    setInputText('')
+  }
+  useEffect(() => {
+    if (connection) {
+      connection
+        .start()
+        .then(() => {
+          connection.on('ReceiveMessage', (message) => {
+            notification.open({
+              message: 'New Notification',
+              description: message
+            })
+          })
+        })
+        .catch((error) => console.log(error))
+    }
+  }, [connection])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Input
+        value={inputText}
+        onChange={(input) => {
+          setInputText(input.target.value)
+        }}
+      />
+      <Button onClick={sendMessage} type='primary'>
+        Send
+      </Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
